94dcfce85cb3b13efc15be7e46be1e6823081fda
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2025-02-18 01:24:23 +0100

    maj du controller avec la gestion des directions

--- playerConctroller.lua
+++ playerConctroller.lua
@@ -2,22 +2,22 @@ local playercontroller = {}
 
 
 -- param entities a qui on veux appliquer une "manette"
-function playercontroller.update(entities)
+function playercontroller.update(dt, entitie)
     --Nous permet d'effectuer une action quand la touche spécifié en param est appuyé
     if love.keyboard.isDown("up") then
-
+        entitie.y = entitie.y - 100 * dt
     end
 
     if love.keyboard.isDown("down") then
-
+        entitie.y = entitie.y + 100 * dt
     end
 
     if love.keyboard.isDown("left") then
-
+        entitie.x = entitie.x - 100 * dt
     end
 
     if love.keyboard.isDown("right") then
-
+        entitie.x = entitie.x + 100 * dt
     end
 end
 
