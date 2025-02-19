964cdb3eec728412295fd02bdea97120184da09d
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2025-02-19 15:36:43 +0100

    ajout de nouvelles constantes sound effect, music et projectile

--- constantes.lua
+++ constantes.lua
@@ -1,5 +1,11 @@
 -- Un module qui nous renvoi une liste de "constantes". ps: simulacre de const en lua cela n'existe pas
 local constantes = {
+     --music
+     MSC_MANSION = love.audio.newSource("/sounds/lavanville_sinister_remix_16bit.wav", "stream"),
+     -- sound effect
+     SND_SHOOT = 0,
+     SND_DESTRUCTION = 0,
+     SND_GROWL = love.audio.newSource("/sounds/ghost_growl.wav", "static"),
      -- Constantes de machine a etat
      NONE = "none",
      WALK = "walk",
@@ -17,7 +23,9 @@ local constantes = {
      MOB_SPRT = "monster_",
      GHOST_SPRT = "ghost_",
      SCREENWIDTH = love.graphics.getWidth(),
-     SCREENHEIGHT = love.graphics.getHeight()
+     SCREENHEIGHT = love.graphics.getHeight(),
+     -- Constantes de projectile
+     PRJTL_ECTOPLASM = love.graphics.newImage("/assets/ectoplasm.png")
     
 }
 
