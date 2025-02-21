7ce9733e21629eaf4e49063c59303baae05bc750
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2025-02-21 21:02:26 +0100

    Creation du game.lua qui gere la pause et event.quit() ainsi que les panels  pause inventaire maj main constante son et assets

--- main.lua
+++ main.lua
@@ -14,15 +14,24 @@ local projectiles = require("/projectile")
 local bubble = require("/stateBubble")
 local life = require("/life")
 local spawner = require("/spawner")
+local game = require("/game")
+local panel = nil
+local groupe = nil
+local text = nil
 local lstProjectiles = {}
 local lstEntities = {}
 local eye = love.graphics.newImage("/assets/eye.png")
+local pause = false
 
 love.graphics.setDefaultFilter("nearest")
+-- love.window.setMode(1920, 1080)
 math.randomseed(os.time())
 
 
 function love.load()
+    -- 📝📝 Font
+    love.graphics.setFont(const.FONT)
+
     -- 🎼🎼 Musique
     bckgrndMusic = const.MSC_MANSION
     bckgrndMusic:setLooping(true)
@@ -33,7 +42,7 @@ function love.load()
 
     -- 🏡🏡spawner
     spawner.spawner()
-    -- 🏡👻👾 spawn mob
+    -- 🏡👻👾 spawn mob no mob spawn 
     spawner.spawn()
 
     -- lstEntities = spawner.entities
@@ -43,57 +52,56 @@ function love.load()
     hero.angle = 0
     table.insert(lstEntities, hero)
 
-    -- 👾👾 On creer un ou des mob(s) et on l'ajoute dans notre liste d'entities
-    -- for nb = 1, 2 do
-    --     zombie = entitie.create(const.MOB)
-    --     table.insert(lstEntities, zombie)
-    -- end
-
-    -- 👻👻 On creer un ou des ghost et on l'ajoute dans notre liste d'entities
-    -- for nb = 1, 1 do
-    --     ghost = entitie.create(const.GHOST)
-    --     table.insert(lstEntities, ghost)
-    -- end
 
 end
 
-
 function love.update(dt)
-    -- 🏡👻👾 update du spawner on fait spawn les ennemis
-    spawner.update(dt, lstEntities)
+    -- gestion du GUI du jeu
+    pause = game.isPaused()
 
-    -- 🏡👻👾 -> On insert nos entities spawner dans notre lstEntities
-    for nb = #spawner.entities, 1, -1 do
-        table.insert(lstEntities, spawner.entities[nb])
-        -- on vide notre spawn entities pour eviter les duplication lié a l'update
-        spawner.entities = {}
-    end
+    if pause  == false then
+
+        love.audio.play(bckgrndMusic)
+
+        -- 🏡👻👾 update du spawner on fait spawn les ennemis
+        spawner.update(dt, lstEntities)
+
+        -- 🏡👻👾 -> On insert nos entities spawner dans notre lstEntities
+        for nb = #spawner.entities, 1, -1 do
+            table.insert(lstEntities, spawner.entities[nb])
+            -- on vide notre spawn entities pour eviter les duplication lié a l'update
+            spawner.entities = {}
+        end
 
 
-    if lstEntities ~= 0 then
-        --on boucle dans notre lstentitie pour lui appliquer son update
-        for nb = 1, #lstEntities do
-            --Animation des framerate des entities dans l'entitite.update
-            entitie.update(dt, lstEntities[nb])
+        if lstEntities ~= 0 then
+            --on boucle dans notre lstentitie pour lui appliquer son update
+            for nb = 1, #lstEntities do
+                --Animation des framerate des entities dans l'entitite.update
+                entitie.update(dt, lstEntities[nb])
 
-            -- on verifie si on a un hero
-            if lstEntities[nb].type == const.HERO then
-                --Si il existe on lui applique une "manette"
-                controller.update(dt, lstEntities[nb])
-            end
+                -- on verifie si on a un hero
+                if lstEntities[nb].type == const.HERO then
+                    --Si il existe on lui applique une "manette"
+                    controller.update(dt, lstEntities[nb])
+                end
 
-            -- on verifie si on a des mobs
-            if lstEntities[nb].type ~= const.HERO then
-                -- si oui on applique notre machine a etats sur nos entities de type mob
-                machine.states(dt, lstEntities[nb], lstEntities)
-                machine.update(dt, lstEntities)
+                -- on verifie si on a des mobs
+                if lstEntities[nb].type ~= const.HERO then
+                    -- si oui on applique notre machine a etats sur nos entities de type mob
+                    machine.states(dt, lstEntities[nb], lstEntities)
+                    machine.update(dt, lstEntities)
+                end
             end
         end
+    else
+        love.audio.pause(bckgrndMusic)
     end
-
 end
 
 function love.draw()
+    -- on affiche tout les gui inventaire et menu pause
+    game.draw()
     -- On boucle dans notre liste d'entities (hero ou mobs) afin de tous les afficher a l'ecran
     for index = 1, #lstEntities do
         machine.draw()
@@ -104,22 +112,21 @@ function love.draw()
         -- on empeche le nil pointer et on affiche le range
         if entitie.range ~= nil then
             --on affiche le rayon du range autours du mob
-            love.graphics.circle("line", entitie.x, entitie.y, entitie.range)
+            -- love.graphics.circle("line", entitie.x, entitie.y, entitie.range)
         end
 
         -- 💭💭 stateBubble
         bubble.state(entitie)
-
 
         -- 🏡🏡 spawner
         spawner.draw()
 
         -- Life entitie, pdv, posX, posY
         if entitie.type == const.HERO then
-            love.graphics.print(entitie.type)
-            love.graphics.print("pdv "..tostring(entitie.life), 400, 400)
+            -- love.graphics.print(entitie.type)
+            -- love.graphics.print("pdv "..tostring(entitie.life), 400, 400)
             love.graphics.draw(const.OBJ_BOW[entitie.currentFrameOBJ], entitie.x + entitie.width, entitie.y + entitie.height, entitie.angleShoot, 1, 1, const.OBJ_BOW[1]:getWidth() / 2, const.OBJ_BOW[1]:getHeight() / 2)
-            life.show(entitie.life, 10, 10)
+            -- life.show(entitie.life, 10, 10)
         end
 
         --debug
@@ -131,8 +138,13 @@ function love.draw()
 end
 
 function love.keypressed(key)
+    game.keypressed(key)
     -- a l'appui de la touche echape on quitte le jeu
     if key == "escape" then
-        love.event.quit()
+        pause = game.pause()
+    end
+
+    if key == "p" then
+       pause = game.pause()
     end
 end
